6faf61767302fdbb7b36433b6564d571001eeac4
deja232 <bil22jas@gmail.com>
2021-05-20 18:11:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/joeys/checkout.java
@@ -0,0 +1,15 @@
+package com.example.joeys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class checkout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout);
+        getSupportActionBar().hide();
+    }
+}
---
+++ app/src/main/java/com/example/joeys/form.java
@@ -0,0 +1,15 @@
+package com.example.joeys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+        getSupportActionBar().hide();
+    }
+}
